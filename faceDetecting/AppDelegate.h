b54f5a3179ca63bdb5889f057cab57ac42fb0370
flysofast <phuoc-de@sumis-iMac-2.local>
2014-10-14 09:20:24 +0700

    Initial Commit

---
+++ faceDetecting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  faceDetecting
+//
+//  Created by phuoc-de on 10/14/14.
+//  Copyright (c) 2014 LeHaiNam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ faceDetecting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  faceDetecting
+//
+//  Created by phuoc-de on 10/14/14.
+//  Copyright (c) 2014 LeHaiNam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
